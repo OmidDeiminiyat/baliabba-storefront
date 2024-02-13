0b53a2192cc97a212a946f7957d8e77e72bd7260
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-02-13 14:22:44 +0100

    Edited

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,12 +1,61 @@
 
-fetch('https://dog.ceo/api/breeds/list/all')  
-.then(response => {
-  return response.json();
-})
-.then(data => {
-  console.log(data);
-  // Handle the retrieved data here
-})
-.catch(error => {
-  console.error('There was a problem with your fetch operation:', error);
-});
+GetProductData()
+
+function GetProductData(){
+    fetch('https://dummyjson.com/products')
+    .then((result) => {
+        return result.json();
+    })
+    .then((json) => {
+        ProductRecived(json);
+    });
+}
+
+
+
+function ProductRecived(ProductData) {
+     console.log(ProductData);
+
+     console.log(ProductData.products[4]);
+     let MyProduct = [];
+     MyProduct.push(ProductData.products[4],ProductData.products[5],ProductData.products[9]);
+     
+
+ const Price = MyProduct[0].price;
+console.log(Price);
+
+const Title = MyProduct[0].title;
+console.log(Title);
+
+const Desctip = MyProduct[0].description;
+console.log(Desctip);
+
+const Image = MyProduct[0].thumbnail;
+console.log(Image);
+    // let myProducts = ProductData.products
+    // console.log(myProducts[8]);
+   const myTitle = document.getElementById('title');
+
+   const image = document.createElement('img');
+   image.src = MyProduct[0].thumbnail;
+   
+   const title = document.createElement('h2');
+      title.textContent = MyProduct[0].title;
+
+      const price = document.createElement('h4');
+      price.textContent = MyProduct[0].price;
+
+      const desc = document.createElement('p');
+      desc.textContent = MyProduct[0].description;
+
+
+
+      myTitle.appendChild(price);
+      myTitle.appendChild(title);
+      myTitle.appendChild(desc);
+      myTitle.appendChild(image);
+    
+
+ 
+    
+}
